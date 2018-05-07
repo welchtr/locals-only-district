c7f9bc657070ffc0de8587ec5c5094b18ed0b992
Tiara Welch <twelch1586@gmail.com>
2018-05-07 18:32:51 -0400

    added a few events

--- eventListApi/models/eventListModels.js
+++ eventListApi/models/eventListModels.js
@@ -4,13 +4,12 @@ var Schema = mongoose.Schema;
 
 
 var EventsSchema = new Schema({
-  // will eventually want a picture piece**
-  event_photo: {type: String},
-  event_name: { type: String },
-   event_venue : { type: String },
-   event_date: { type: String,},
-   event_time: { type: String },
-   event_link: { type: String }
+   event_photo: {type: String},
+   event_name:  {type: String},
+   event_venue: {type: String},
+   event_date:  {type: String},
+   event_time:  {type: String},
+   event_link:  {type: String}
 });
 
 module.exports = mongoose.model('Events', EventsSchema);
